175c1dc97f38a7c080d1cdc5c53b5eebb3b995ae
Carolyn Sun <carolyn.sun@duke.edu>
2018-09-15 20:32:56 -0400

    pub

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+Vue.use(Vuetify)
+import App from './App.vue'
+import VuetifyClipboard from 'vuetify-clipboard-input';
+Vue.use(VuetifyClipboard);
